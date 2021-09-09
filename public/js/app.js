fba3a28542e0dc44be381435e798d5b200eca867
MLHocalhost <18cy039@esisalama.org>
2021-09-09 17:33:18 +0200

    feat (optimize)

--- public/js/app.js
+++ public/js/app.js
@@ -7,46 +7,49 @@ let indicatorRight = tabIndicator.getBoundingClientRect().left;
 let tabContents = document.querySelectorAll('.tab-content');
 let componentContainers = document.querySelectorAll('.component-container')
 
+let sideBtn = document.querySelector('#docs')
+let sideBar = document.querySelector('#sidebar')
+
+sideBtn.addEventListener('click', ()=>{
+    if (sideBar.classList.contains('-translate-x-96')){
+        sideBar.classList.replace('-translate-x-96', 'translate-x-0')
+    }else{
+        sideBar.classList.replace('translate-x-0', '-translate-x-96')
+    }
+})
+
+
 document.addEventListener('DOMContentLoaded', ()=>{
     let tabItemsProps = tabItems[0].getBoundingClientRect();
-    tabItems[0].classList.add('text-white');
     tabIndicator.style.height = tabItemsProps.height + "px";
     tabIndicator.style.width = tabItemsProps.width + "px";
 })  
 
-componentContainers.forEach(componentContainer =>{
-    
-    tabItems.forEach(tabItem => {
-        tabItem.addEventListener('click', ()=>{
-            let tabsize = tabItem.getBoundingClientRect();
-            let tabTarget = tabItem.getAttribute('data-target')
-    
-            tabIndicator.style.width = tabsize.width +"px";
-            tabIndicator.style.height = tabsize.height + "px";
-            tabIndicator.style.left = (tabsize.left - tabProps.left) + "px";
-    
-            if (tabItem.getAttribute('data-target') == "code"){
-                componentContainer.classList.replace('bg-white', 'bg-gray-800');
-            }else {
-                componentContainer.classList.replace('bg-gray-800','bg-white');
-            }
-    
-            tabContents.forEach(tabContent =>{
-    
-                let tabName = tabContent.getAttribute('data-name')
-    
-                if(tabTarget == tabName){
-                    if (tabContent.classList.contains('invisible')){
-                        tabContent.classList.replace('opacity-0', 'opacity-100');
-                        tabContent.classList.replace('invisible', 'visible');
-                        tabContent.classList.replace('-translate-y-10', 'translate-y-0');
-                    }
-                }else{
-                    tabContent.classList.replace('opacity-100', 'opacity-0');
-                    tabContent.classList.replace('visible', 'invisible');
-                    tabContent.classList.replace('translate-y-0', '-translate-y-10');
-                }
+tabItems.forEach(tabItem => {
+    tabItem.addEventListener('click', ()=>{
+        let tabsize = tabItem.getBoundingClientRect();
+        let tabTarget = tabItem.getAttribute('data-target')
+
+        tabIndicator.style.width = tabsize.width +"px";
+        tabIndicator.style.height = tabsize.height + "px";
+        tabIndicator.style.left = (tabsize.left - tabProps.left) + "px";
+
+        tabContents.forEach(tabContent =>{
+            let tabName = tabContent.getAttribute('data-name')
+            componentContainers.forEach(componentContainer =>{
+                componentContainer.style.height = tabContent.getBoundingClientRect().height + "px"
             })
+            if(tabTarget == tabName){
+                if (tabContent.classList.contains('invisible')){
+                    tabContent.classList.replace('opacity-0', 'opacity-100');
+                    tabContent.classList.replace('invisible', 'visible');
+                    tabContent.classList.replace('-translate-y-10', 'translate-y-0');
+                }
+            }else{
+                tabContent.classList.replace('opacity-100', 'opacity-0');
+                tabContent.classList.replace('visible', 'invisible');
+                tabContent.classList.replace('translate-y-0', '-translate-y-10');
+            }
         })
-    });
-})
+    })
+});
